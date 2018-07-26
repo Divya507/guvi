0751d83fd09ad07c3c4091a719c9e23af1c9dd17
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-07-26 20:46:40 +0530

    Create ex9.java

---
+++ ex9.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0,n,i;
+		Scanner Sc=new Scanner(System.in);
+		n=Sc.nextInt();
+for(i=1;i<+n;++i)	
+{
+	sum+=i;
+}
+System.out.println(sum);
+	}
+}
