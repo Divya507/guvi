684b8042112bc7bb8c59e47654df5bbebd1fac73
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-07-31 21:24:51 +0530

    Create divya6.java

---
+++ divya6.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,d,n;
+		Scanner Sc=new Scanner(System.in);
+		a=Sc.nextInt();
+		d=Sc.nextInt();
+		n=Sc.nextInt();
+		System.out.println(n*((2*a)+(n-1)*d)/2);
+	}
+}
