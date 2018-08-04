6bc6a7b03ca21f8764c7325ac9b20cf82ab90d0e
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-04 13:47:19 +0530

    Create divya10.java

---
+++ divya10.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,n;
+		Scanner Sc=new Scanner(System.in);
+		n=Sc.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=Sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			System.out.println(a[i]+"\t"+i);
+		}
+		}
+}
