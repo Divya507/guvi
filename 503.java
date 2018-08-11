fc430b87cb4be601c470f1f6727207ba0d0b8c46
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-11 16:21:29 +0530

    Create 503.java

---
+++ 503.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		int n,i;
+		Scanner Sc=new Scanner(System.in);
+		str=Sc.next();
+		n=Sc.nextInt();
+		for(i=0;i<=n;i++)
+		{
+			System.out.println(str);
+		}
+		
+	}
+}
