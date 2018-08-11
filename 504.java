7013cd7c0e8678539719c7b3e4698574cf31a5ca
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-11 16:34:47 +0530

    Create 504.java

---
+++ 504.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int num;
+		Scanner Sc=new Scanner(System.in);
+		num=Sc.nextInt();
+		if(num==1 || num==2|| num==3||num==4||num==5||num==6||num==7||num==8||num==9||num==10)
+		{
+			System.out.println("Yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+	}
+}
