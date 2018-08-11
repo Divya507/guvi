b5593ab3b36350180b1abc084f50b7bde0ae262d
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-11 12:50:51 +0530

    Create 502.java

---
+++ 502.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1,n2,n;
+		Scanner s=new Scanner(System.in);
+		n1=s.nextInt();
+		n2=s.nextInt();
+		n=n1+n2;
+		System.out.println(n);
+	}
+}
