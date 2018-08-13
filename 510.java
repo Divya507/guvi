edd63b38ea736f15e488acceb6b287376e1e5400
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-13 11:28:09 +0530

    Create 510.java

---
+++ 510.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str1,str2,str3;
+		Scanner Sc=new Scanner(System.in);
+		str1=Sc.nextLine();
+		str2=Sc.nextLine();
+		str3=str1+str2;
+		System.out.println(str3);
+	}
+}
