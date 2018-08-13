3a59fd80610ffafab5b3a34c87da1077857e9865
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-13 20:05:58 +0530

    Create 514.java

---
+++ 514.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,m,d;
+		Scanner Sc=new Scanner(System.in);
+		n=Sc.nextInt();
+		m=Sc.nextInt();
+		d=n*m;
+		if(d%2==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
