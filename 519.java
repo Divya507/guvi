af0976d49d3b1bae3b7bffb30f2e9c55db66eb6e
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-14 12:53:47 +0530

    Create 519.java

---
+++ 519.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,k,i,count=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(k==a[i])
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
