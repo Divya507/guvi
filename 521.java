35fb0005bc424b7f936c88f4e4d73be041d4695a
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-14 14:02:23 +0530

    Create 521.java

---
+++ 521.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s;
+		int i,k;
+		Scanner sc=new Scanner(System.in);
+		s=sc.next();
+		k=sc.nextInt();
+		char[] ch=s.toCharArray();
+		for(i=0;i<k;i++)
+		{
+			System.out.println("\t"+ch[i]+"\t");
+		}
+		
+	}
+}
