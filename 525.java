c2cf9299ef4c759a5d4715bd831a159ac6bbef3f
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-14 16:48:37 +0530

    Create 525.java

---
+++ 525.java
@@ -0,0 +1,32 @@
+ 
+ 
+ import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ class ideone 
+
+{
+
+     public static void main(String[] args) 
+
+     {
+
+        int i = 0;
+        String str;
+        Scanner s=new Scanner(System.in);
+        str=s.next();
+
+        if(str == null) {}
+
+        for(i = 1; i <=1;System.out.println(str))
+
+        {
+
+            i++;
+
+     	}
+
+     }
+
+}
