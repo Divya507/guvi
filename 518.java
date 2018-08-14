cb53add768241ec175a1c87c20958dd98264c75c
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-14 12:10:15 +0530

    Create 518.java

---
+++ 518.java
@@ -0,0 +1,54 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int ch;
+		Scanner s=new Scanner(System.in);
+		ch=s.nextInt();
+		switch(ch)
+		{
+		case 1:
+			System.out.println("One");
+			break;
+			case 2:
+				System.out.println("Two");
+				break;
+				case 3:
+					System.out.println("Three");
+					break;
+					case 4:
+						System.out.println("Four");
+						break;
+						case 5:
+							System.out.println("Five");
+							break;
+							case 6:
+								System.out.println("Six");
+								break;
+								case 7:
+									System.out.println("Seven");
+									break;
+									case 8:
+										System.out.println("Eight");
+										break;
+										case 9:
+											System.out.println("Nine");
+											break;
+											case 10:
+												System.out.println("Ten");
+												break;
+												default:
+												break;
+												
+			
+	}
+}
+}
