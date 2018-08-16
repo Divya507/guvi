ec47945bf288ad546e060306d0ea7f005504585b
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-16 21:16:40 +0530

    Create 66.java

---
+++ 66.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		        int num;
+		        Scanner s=new Scanner(System.in);
+		        num=s.nextInt();
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+
+	}
+}
