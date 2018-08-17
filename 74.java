b1c5b26d3f027c96201f16b83e3e32a1aae422ec
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-17 12:57:17 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		float n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextFloat();
+	int sum=(int) n+1;
+	System.out.println(sum);
+	}
+}
