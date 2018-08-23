cde222223febea2d92c819981e8a5f83f505d3c9
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-23 20:49:58 +0530

    Create 92.java

---
+++ 92.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,i,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+	}
+}
