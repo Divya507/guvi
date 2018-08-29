9c60b83ecf99f4d9defb0ed72c10746532b72cfd
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-08-29 16:13:44 +0530

    Create 104.java

---
+++ 104.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,k,p;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+		p=n;
+		for(int i=0;i<k-1;i++)
+		{
+			n=p*n;
+		}
+		System.out.println(n);
+	}
+}
