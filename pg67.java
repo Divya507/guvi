59957f615cd47bd10f4be3555aa4d8dde3a7ce20
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-10-05 15:15:25 +0530

    Create pg67.java

---
+++ pg67.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,fact=1;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		while(n>0)
+		{
+			fact=fact*n;
+			n--;
+		}
+		System.out.println(fact);
+	}
+}
