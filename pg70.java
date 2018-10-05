664e79f0af36fd72c79aae1973760de3213be41f
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-10-05 15:30:12 +0530

    Create pg70.java

---
+++ pg70.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		String str1="Answer";
+		System.out.println(str+" "+str1);
+	}
+}
