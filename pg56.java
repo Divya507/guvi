a5e40352d2837848d32ba0637aafb195a50f2fa3
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-10-05 11:16:34 +0530

    Create pg56.java

---
+++ pg56.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str1;
+		int i,n;
+		char p;
+		Scanner s=new Scanner(System.in);
+		str1=s.next();
+		p=s.next().charAt(0);
+		n=str1.length();
+		char[] ch=str1.toCharArray();
+		for(i=0;i<n;i++)
+		{
+			if(p==ch[i])
+			{
+				System.out.println(i+1);
+			}
+		}
+	}
+}
