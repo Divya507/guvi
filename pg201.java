abf78990098e9e48044f79b5dd1e51631075b567
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-10-09 15:55:57 +0530

    Create pg201.java

---
+++ pg201.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class CatalanNumbers {
+   public static long fact(int i) {
+      if(i <= 1) {
+         return 1;
+      }
+      return i * fact(i - 1);
+   }
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      
+      int num = sc.nextInt();
+      
+      //(2n)!/(n+1)!*n!
+      for(int i = 0; i<=num; i++) {
+         long Cn = (fact(2*i))/(fact(i+1)*fact(i));
+         System.out.println(Cn+"\t");
+      }
+   }
+}
