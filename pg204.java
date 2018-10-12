4e0eefac19a039ae240aa4b62ec8f04a85537f45
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-10-12 15:42:37 +0530

    Create pg204.java

---
+++ pg204.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,i,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(a[i]<0)
+			{
+				sum=sum+a[i];
+				
+			}
+		}
+		System.out.println(sum);
+		
+	}
+}
